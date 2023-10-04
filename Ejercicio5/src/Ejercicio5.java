1dc51f2ce9601b61dc93cf5168d2a67bbdc05af3
alumnofp <alumnofp@FPA13.CALASANZSALAMANCA.local>
2023-10-04 10:57:01 +0200

    Bomba en casa

--- Ejercicio5/src/Ejercicio5.java
+++ Ejercicio5/src/Ejercicio5.java
@@ -1,4 +1,23 @@
 
 public class Ejercicio5 {
+	public  static void main(String[] args){
+		int menos5 = -5;
+		int mas8 = 8;
+		int mas6 = 6;
+		int mas55 = 55;
+		int mas9 = 9;
+		int mas20 = 20;
+		int menos3 = -3;
+		int mas5 = 5;
+		int mas15 = 15;
+		int mas3 = 3;
+		int mas2 = 2;
+		int menos8 = -8;
+		
+		System.out.println("El primer resultado: " + menos5 + "+" + mas8 + "*" + mas6 + "es: " + menos5 + mas8 * mas6);
+		
+		
+		
 
+	}
 }
